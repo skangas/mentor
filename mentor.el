074bd57a1e19d7807d682552fee63f326d1ad05c
Stefan Kangas <skangas@skangas.se>
2017-01-02 09:28:04 +0100

    Bump version to 0.3

--- mentor.el
+++ mentor.el
@@ -8,7 +8,7 @@
 ;; Keywords: comm, processes, bittorrent
 ;; Package-Requires: ((xml-rpc "1.6.9") (seq) (cl-lib "0.5"))
 
-(defconst mentor-version "0.2"
+(defconst mentor-version "0.3"
   "The version of Mentor that you're using.")
 
 ;; This file is NOT part of GNU Emacs.
@@ -28,9 +28,10 @@
 
 ;;; Commentary:
 
-;; mentor is a GNU Emacs frontend for the `rTorrent' bittorrent client.  It uses
-;; XML-RPC to communicate with rTorrent, and needs rTorrent to be configured
-;; accordingly.
+;; mentor is a GNU Emacs frontend for the `rTorrent' bittorrent client.
+
+;; By default, it will start and run rTorrent from within Emacs but can also be
+;; configured to use an external rTorrent instance over XML-RPC.
 
 ;; This project aims to provide a feature complete and highly customizable
 ;; interface, which will feel familiar to Emacs users.  Key bindings are chosen
