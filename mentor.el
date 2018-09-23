4393f8981a46ede17dc873e757cb8b986c65bb81
Stefan Kangas <stefankangas@gmail.com>
2018-09-23 17:52:36 +0200

    Add async version to package header

--- mentor.el
+++ mentor.el
@@ -6,7 +6,7 @@
 ;; Author: Stefan Kangas <stefankangas@gmail.com>
 ;; Version: 0.3.2
 ;; Keywords: comm, processes, bittorrent
-;; Package-Requires: ((xml-rpc "1.6.9") (seq "1.11") (cl-lib "0.5") (async))
+;; Package-Requires: ((xml-rpc "1.6.9") (seq "1.11") (cl-lib "0.5") (async "1.9.3"))
 
 (defconst mentor-version "0.3.2"
   "The version of Mentor that you're using.")
