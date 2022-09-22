9508abcd313feeef28f780a6d0061efd8d2f21ef
Stefan Kangas <stefankangas@gmail.com>
2022-09-22 16:08:34 +0200

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode . ((indent-tabs-mode . nil))))
+((nil . ((sentence-end-double-space . t)))
+ (emacs-lisp-mode . ((indent-tabs-mode . nil)))
+ (lisp-data-mode . ((indent-tabs-mode . nil))))
