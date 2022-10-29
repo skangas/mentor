0ca84afe9d5d8e3196d67caac304f58752bd7f49
Stefan Kangas <stefankangas@gmail.com>
2022-10-29 17:52:11 +0200

    Require url-scgi 0.8

--- mentor.el
+++ mentor.el
@@ -7,7 +7,7 @@
 
 ;; Version: 0.5
 ;; Keywords: comm, processes, bittorrent
-;; Package-Requires: ((emacs "25.1") (xml-rpc "1.6.15") (seq "1.11") (async "1.9.3") (url-scgi "0.7"))
+;; Package-Requires: ((emacs "25.1") (xml-rpc "1.6.15") (seq "1.11") (async "1.9.3") (url-scgi "0.8"))
 ;; URL: https://github.com/skangas/mentor
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
