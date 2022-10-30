089df9b6d0cb8289cb1226ca6dce45ab6c9ce43b
Stefan Kangas <stefankangas@gmail.com>
2022-10-30 14:34:28 +0100

    Add missing "/" to gnu.org URL

--- mentor-files.el
+++ mentor-files.el
@@ -17,7 +17,7 @@
 ;; License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with Mentor.  If not, see <https://www.gnu.org/licenses>.
+;; along with Mentor.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
